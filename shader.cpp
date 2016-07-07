a36d84a75c866fa01437db00556da3eb7bb6fb03
Jeff Souza <jeffreymsouza@gmail.com>
2016-07-07 16:11:09 -0600

    Fix comment

--- shader.cpp
+++ shader.cpp
@@ -81,7 +81,7 @@ int main() {
 		glVertexAttribPointer( 0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( GLfloat ), (GLvoid*)0 );
 		glEnableVertexAttribArray( 0 );
 
-		// Poisiton attributes
+		// Color attributes
 		glVertexAttribPointer( 1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof( GLfloat ), (GLvoid*)(3 * sizeof( GLfloat )) );
 		glEnableVertexAttribArray( 1 );
 
